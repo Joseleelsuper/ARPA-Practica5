bb5b487d6edf6c6555836bb5f65b703a9e893cac
José Gallardo <tory002254@gmail.com>
2024-10-17 20:06:48 +0200

    a

--- ARPA-Practica5/Practica5.cpp
+++ ARPA-Practica5/Practica5.cpp
@@ -1,13 +1,12 @@
 #include <mpi.h>
 #include <iostream>
-#include <fstream>
 #include <vector>
 
 using namespace std;
 
 constexpr int RANK_MASTER = 0;      // Rango del proceso maestro
-constexpr int NUM_DATOS = 3;        // Número de datos a enviar (3 veces)
-constexpr const char* NOMBRE_FICHERO = "a.dat";  // Nombre del fichero
+constexpr int NUM_DATOS = 3;        // Número de datos a escribir y leer
+const char* NOMBRE_FICHERO = "Practica5.dat";  // Nombre del fichero
 
 int main(int argc, char* argv[]) {
     MPI_Init(&argc, &argv);
@@ -18,25 +17,29 @@ int main(int argc, char* argv[]) {
 
     double startTime = MPI_Wtime();
 
+	// Inicializamos el fichero
     MPI_File fh;
-    MPI_File_open(MPI_COMM_WORLD, const_cast<char*>(NOMBRE_FICHERO), MPI_MODE_CREATE | MPI_MODE_RDWR, MPI_INFO_NULL, &fh);
+    MPI_File_open(
+        MPI_COMM_WORLD, 
+        const_cast<char*>(NOMBRE_FICHERO), 
+        MPI_MODE_CREATE | MPI_MODE_WRONLY, 
+        MPI_INFO_NULL, 
+        &fh
+    );
 
     // Escribir el rango del proceso en el fichero 3 veces
-    char write_values[NUM_DATOS]{};
+    char write_values[NUM_DATOS];
     for (int i = 0; i < NUM_DATOS; ++i) {
         write_values[i] = rank + 48; // Convertir el rango a ASCII
     }
-
     MPI_Offset offset = rank * NUM_DATOS * sizeof(char);
     MPI_File_write_at(fh, offset, write_values, NUM_DATOS, MPI_CHAR, MPI_STATUS_IGNORE);
-
     MPI_File_close(&fh);
 
     // Leer los valores del fichero
     MPI_File_open(MPI_COMM_WORLD, const_cast<char*>(NOMBRE_FICHERO), MPI_MODE_RDONLY, MPI_INFO_NULL, &fh);
     char read_values[NUM_DATOS];
     MPI_File_read_at(fh, offset, read_values, NUM_DATOS, MPI_CHAR, MPI_STATUS_IGNORE);
-
     MPI_File_close(&fh);
 
     // Recolectar los valores leídos en el proceso maestro
@@ -54,7 +57,8 @@ int main(int argc, char* argv[]) {
             }
             printf("del fichero.\n");
         }
-        printf("Tiempo de ejecución: %f segundos\n", endTime - startTime);
+        printf("\nTiempo de ejecución: %f segundos\n", endTime - startTime);
+        system("notepad Practica5.dat");
     }
 
     MPI_Finalize();
